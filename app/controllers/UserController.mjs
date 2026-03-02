798e2a6c2173135289cb67978bdca26a1757ce26
StepCode3630 <196639646+StepCode3630@users.noreply.github.com>
2026-03-02 15:01:09 +0100

    feat:auth

--- app/controllers/UserController.mjs
+++ app/controllers/UserController.mjs
@@ -1,16 +1,15 @@
 import express from "express";
 
 // Import du module jwt
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 export const get = (req, res) => {
-
-    // Remplacez cette portion de code par votre traitement du jeton JWT
-    return res.status(418).json({error: 'Je suis une théière'});
-
+  // Remplacez cette portion de code par votre traitement du jeton JWT
+  return res.status(418).json({ error: "Salam" });
 };
 
-router.get('/', get);
+router.get("/", get);
 
 export default router;
--- app/routes/Auth.mjs
+++ app/routes/Auth.mjs
@@ -1,43 +1,49 @@
 import express from "express";
-import {connectToDatabase} from "../utils/dbUtils.mjs";
-
+import { connectToDatabase } from "../utils/dbUtils.mjs";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
+const secretKey = "hi";
+
 // Middleware pour la connexion à la base de données
 const connectToDatabaseMiddleware = async (req, res, next) => {
-    try {
-        req.dbConnection = await connectToDatabase();
-        next();
-    } catch (error) {
-        console.error("Error connecting to the database:", error);
-        res.status(500).json({error: "Internal Server Error"});
-    }
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(403).send("Accès refusé. Token non fourni.");
+
+  try {
+    req.dbConnection = await connectToDatabase();
+    next();
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
+router.post("/", connectToDatabaseMiddleware, async (req, res) => {
+  const { username, password } = req.body;
 
-router.post('/', connectToDatabaseMiddleware, async (req, res) => {
-    const {username, password} = req.body;
-
-    const queryString = `SELECT *
+  const queryString = `SELECT *
                          FROM t_users
                          WHERE useName = '${username}'
                            AND usePassword = '${password}'`;
 
-    try {
-        const [rows] = await req.dbConnection.query(queryString, [username, password]);
-        if (rows.length > 0) {
-
-            // signer et renvoyer votre token ici (utiliser un code http de succès)
-
-            res.status(200).json({message: "Authentication successful"});
-        } else {
-            res.status(401).json({error: "Invalid username or password"});
-        }
-    } catch (error) {
-        console.error("Error authenticating user:", error);
-        res.status(500).json({error: "Internal Server Error"});
+  try {
+    const [rows] = await req.dbConnection.query(queryString, [
+      username,
+      password,
+    ]);
+    if (rows.length > 0) {
+      // signer et renvoyer votre token ici (utiliser un code http de succès)
+      const token = jwt.sign({ username }, secretKey, { expiresIn: "1h" });
+      res.status(200).json({ token });
+    } else {
+      res.status(401).json({ error: "Invalid username or password" });
     }
+  } catch (error) {
+    console.error("Error authenticating user:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 export default router;
